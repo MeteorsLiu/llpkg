f91cfb483fff8980c9565444e34f6fce4f0c44c9
Rick Guo <coopticks@outlook.com>
2025-03-25 12:59:38 +0800

    Cargs (#35)
    * Release-as: cargs/v0.1.0
    
    * Release-as: cargs/v0.1.0
    
    ---------
    
    Co-authored-by: Heuluck Lu <sonicth@foxmail.com>

---
+++ cargs/_demo/test/test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	C "github.com/goplus/llgo/c"
+	"github.com/goplus/llpkg/cargs"
+)
+
+func main() {
+	// Define command-line options
+	options := []cargs.CagOption{
+		{
+			Identifier:    'h',
+			AccessLetters: C.Str("h"),
+			AccessName:    C.Str("help"),
+			ValueName:     nil,
+			Description:   C.Str("Show help information"),
+		},
+		{
+			Identifier:    'v',
+			AccessLetters: C.Str("v"),
+			AccessName:    C.Str("version"),
+			ValueName:     nil,
+			Description:   C.Str("Show version information"),
+		},
+	}
+
+	args := os.Args
+
+	// Convert Go string array to C-style argv
+	argv := make([]*int8, len(args))
+	for i, arg := range args {
+		argv[i] = C.AllocaCStr(arg)
+	}
+
+	// Initialize option context
+	var context cargs.CagOptionContext
+	context.CagOptionInit(&options[0], uintptr(len(options)), C.Int(len(args)), &argv[0])
+
+	// Process all options
+	for context.CagOptionFetch() {
+		identifier := context.CagOptionGetIdentifier()
+		switch identifier {
+		case 'h':
+			fmt.Println("Help: This is a simple command-line parser demo")
+		case 'v':
+			fmt.Println("Version: 1.0.0")
+		}
+	}
+}
